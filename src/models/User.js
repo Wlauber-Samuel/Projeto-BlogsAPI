490bdcecf3c6644daf3eb13b9d46c9cb1ab0f0e0
Wlauber <wlaubersamuel@gmail.com>
2023-07-13 18:15:06 -0300

    requisito 02 concluído

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const SchemaUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+    underscored: true,
+  });
+
+  return User;
+};
+
+module.exports = SchemaUser;
